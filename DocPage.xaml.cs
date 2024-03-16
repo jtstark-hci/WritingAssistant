09ff18064546d234ef2d807eaceec2a6566d6b18
jtstark-hci <jtstark@dgp.toronto.edu>
2024-03-16 16:38:01 -0400

    More UI elements added

--- DocPage.xaml.cs
+++ DocPage.xaml.cs
@@ -6,6 +6,8 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Text;
+using Windows.UI;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,6 +15,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using Windows.Storage;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -33,6 +38,126 @@ namespace WritingAssistant
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(EntryPage), null, new SuppressNavigationTransitionInfo());
+        }
+
+
+        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open a text file.
+            Windows.Storage.Pickers.FileOpenPicker open =
+                new Windows.Storage.Pickers.FileOpenPicker();
+            open.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            open.FileTypeFilter.Add(".rtf");
+
+            Windows.Storage.StorageFile file = await open.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                using (Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    // Load the file into the Document property of the RichEditBox.
+                    editor.Document.LoadFromStream(Microsoft.UI.Text.TextSetOptions.FormatRtf, randAccStream);
+                }
+            }
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Rich Text", new List<string>() { ".rtf" });
+
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until we
+                // finish making changes and call CompleteUpdatesAsync.
+                CachedFileManager.DeferUpdates(file);
+                // write to file
+                using (Windows.Storage.Streams.IRandomAccessStream randAccStream =
+                    await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite))
+                {
+                    editor.Document.SaveToStream(Microsoft.UI.Text.TextGetOptions.FormatRtf, randAccStream);
+                }
+
+                // Let Windows know that we're finished changing the file so the
+                // other app can update the remote version of the file.
+                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
+                if (status != FileUpdateStatus.Complete)
+                {
+                    Windows.UI.Popups.MessageDialog errorBox =
+                        new Windows.UI.Popups.MessageDialog("File " + file.Name + " couldn't be saved.");
+                    await errorBox.ShowAsync();
+                }
+            }
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Bold = FormatEffect.Toggle;
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Italic = FormatEffect.Toggle;
+        }
+
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Extract the color of the button that was clicked.
+            Button clickedColor = (Button)sender;
+            var rectangle = (Microsoft.UI.Xaml.Shapes.Rectangle)clickedColor.Content;
+            var color = ((Microsoft.UI.Xaml.Media.SolidColorBrush)rectangle.Fill).Color;
+
+            editor.Document.Selection.CharacterFormat.ForegroundColor = color;
+
+            fontColorButton.Flyout.Hide();
+            editor.Focus(Microsoft.UI.Xaml.FocusState.Keyboard);
+        }
+
+
+        private void Editor_GotFocus(object sender, RoutedEventArgs e)
+        {
+            editor.Document.GetText(TextGetOptions.UseCrlf, out _);
+
+
+        }
+
+        private void Editor_TextChanged(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void charProfIcon_Click(object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = true;
+        }
+
+        private void asstIcon_Click (object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = true;
+        }
+
+        private void panelIcon_Click(object sender, RoutedEventArgs e)
+        {
+            if (!splitView.IsPaneOpen)
+            {
+                splitView.IsPaneOpen = true;
+                panelIcon.Glyph = "\uE89F";
+
+            } else
+            {
+                splitView.IsPaneOpen = false;
+                panelIcon.Glyph = "\uE8A0";
+            }
+
         }
     }
 }

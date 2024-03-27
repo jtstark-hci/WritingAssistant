07b34bb38584c4edd67cf7b5f6a30956068f2485
jtstark-hci <jtstark@dgp.toronto.edu>
2024-03-27 17:29:27 -0400

    set up tab view

--- DocPage.xaml.cs
+++ DocPage.xaml.cs
@@ -20,6 +20,7 @@ using Windows.Storage.Pickers;
 using Windows.Storage.Provider;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using System.Runtime.Serialization;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -33,13 +34,12 @@ namespace WritingAssistant
     public sealed partial class DocPage : Page
     {
 
-        Comment comment = null;
+        //Comment comment = null;
 
         public DocPage()
         {
             this.InitializeComponent();
         }
-
 
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
@@ -56,136 +56,36 @@ namespace WritingAssistant
                     App.SaveNewProject(project);
                     project.isNew = false;
                 }
-                
 
-                foreach(string file in project.StoryFiles)
+
+                foreach (string file in project.StoryFiles)
                 {
                     Debug.WriteLine("found a file");
                     StorageFile f = await StorageFile.GetFileFromPathAsync(file);
+                    TextBlock fileName = new TextBlock();
+                    fileName.Text = f.DisplayName;
+                    StoryFilesList.Children.Add(fileName);
+
                     using (IRandomAccessStream randAccStream =
                         await f.OpenAsync(FileAccessMode.Read))
                     {
-                        Debug.WriteLine("loading file");
-                        editor.Document.LoadFromStream(TextSetOptions.FormatRtf, randAccStream);
-                        docTitle.Text = f.DisplayName;
+                        //editor.Document.LoadFromStream(TextSetOptions.FormatRtf, randAccStream);
+                        //docTitle.Text = f.DisplayName;
                     }
                 }
             }
         }
 
-
-
-        private async void OpenButton_Click(object sender, RoutedEventArgs e)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            // Open a text file.
-            FileOpenPicker opener = new FileOpenPicker();
 
-            var window = App.m_window;
-
-            // Retrieve the window handle (HWND) of the current WinUI 3 window.
-            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-
-            // Initialize the file picker with the window handle (HWND).
-            WinRT.Interop.InitializeWithWindow.Initialize(opener, hWnd);
-
-
-            opener.SuggestedStartLocation =
-                PickerLocationId.DocumentsLibrary;
-            opener.FileTypeFilter.Add(".rtf");
-            opener.FileTypeFilter.Add(".txt");
-
-            StorageFile file = await opener.PickSingleFileAsync();
-
-            if (file != null)
-            {
-                using (IRandomAccessStream randAccStream =
-                    await file.OpenAsync(FileAccessMode.Read))
-                {
-                    // Load the file into the Document property of the RichEditBox.
-                    docTitle.Text = file.DisplayName;
-                    editor.Document.LoadFromStream(TextSetOptions.FormatRtf, randAccStream);
-
-                    //add file to recents (if not already there)
-                }
-            }
         }
 
-        private async void SaveButton_Click(object sender, RoutedEventArgs e)
-        {
-            FileSavePicker savePicker = new FileSavePicker();
-
-            var window = App.m_window;
-
-            // Retrieve the window handle (HWND) of the current WinUI 3 window.
-            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-
-            // Initialize the file picker with the window handle (HWND).
-            WinRT.Interop.InitializeWithWindow.Initialize(savePicker, hWnd);
-
-
-            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
-
-            // Dropdown of file types the user can save the file as
-            savePicker.FileTypeChoices.Add("Rich Text", new List<string>() { ".rtf" });
-
-            // Default file name if the user does not type one in or select a file to replace
-            savePicker.SuggestedFileName = docTitle.Text;
-
-            StorageFile file = await savePicker.PickSaveFileAsync();
-            if (file != null)
-            {
-                // Prevent updates to the remote version of the file until we
-                // finish making changes and call CompleteUpdatesAsync.
-                CachedFileManager.DeferUpdates(file);
-                // write to file
-                using (IRandomAccessStream randAccStream =
-                    await file.OpenAsync(FileAccessMode.ReadWrite))
-                {
-                    editor.Document.SaveToStream(TextGetOptions.FormatRtf, randAccStream);
-                }
-
-                // Let Windows know that we're finished changing the file so the
-                // other app can update the remote version of the file.
-                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
-                if (status != FileUpdateStatus.Complete)
-                {
-                    Windows.UI.Popups.MessageDialog errorBox =
-                        new Windows.UI.Popups.MessageDialog("File " + file.Name + " couldn't be saved.");
-                    await errorBox.ShowAsync();
-                } else
-                {
-                    
-                }
-            }
-        }
-
-        private void BoldButton_Click(object sender, RoutedEventArgs e)
-        {
-            editor.Document.Selection.CharacterFormat.Bold = FormatEffect.Toggle;
-        }
-
-        private void ItalicButton_Click(object sender, RoutedEventArgs e)
-        {
-            editor.Document.Selection.CharacterFormat.Italic = FormatEffect.Toggle;
-        }
-
-        private void ColorButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Extract the color of the button that was clicked.
-            Button clickedColor = (Button)sender;
-            var rectangle = (Microsoft.UI.Xaml.Shapes.Rectangle)clickedColor.Content;
-            var color = ((SolidColorBrush)rectangle.Fill).Color;
-
-            editor.Document.Selection.CharacterFormat.ForegroundColor = color;
-
-            fontColorButton.Flyout.Hide();
-            editor.Focus(FocusState.Keyboard);
-        }
 
 
         private void Editor_GotFocus(object sender, RoutedEventArgs e)
         {
-            editor.Document.GetText(TextGetOptions.UseCrlf, out _);
+            
 
 
         }
@@ -195,48 +95,24 @@ namespace WritingAssistant
             
         }
 
-        private void charProfIcon_Click(object sender, RoutedEventArgs e)
+        //    //testing
+        //    ITextRange range = editor.Document.GetRange(5, 5);
+        //    range.CharacterFormat.Hidden = FormatEffect.On;
+        //    range.SetText(TextSetOptions.FormatRtf, "test insertion");
+        //    Console.WriteLine(range.StartPosition);
+
+
+
+
+
+        private void RoboButton_Click(object sender, RoutedEventArgs e)
         {
-            splitView.IsPaneOpen = true;
-
-            //testing
-            ITextRange range = editor.Document.GetRange(5, 5);
-            range.CharacterFormat.Hidden = FormatEffect.On;
-            range.SetText(TextSetOptions.FormatRtf, "test insertion");
-            Console.WriteLine(range.StartPosition);
-        }
-
-        private void commentIcon_Click (object sender, RoutedEventArgs e)
-        {
-            splitView.IsPaneOpen = true;
-        }
-
-        private void panelIcon_Click(object sender, RoutedEventArgs e)
-        {
-            if (!splitView.IsPaneOpen)
-            {
-                splitView.IsPaneOpen = true;
-                panelIcon.Glyph = "\uE89F";
-
-            } else
-            {
-                splitView.IsPaneOpen = false;
-                panelIcon.Glyph = "\uE8A0";
-            }
 
         }
 
-        private void TitleCancelButton_Click(object sender, RoutedEventArgs e)
+        private void tabsView_TabCloseRequested(TabView sender, TabViewTabCloseRequestedEventArgs args)
         {
-            titleEditBox.Text = "";
-            editTitleFlyout.Hide();
-        }
-
-        private void TitleConfirmButton_Click(object sender, RoutedEventArgs e)
-        {
-            docTitle.Text = titleEditBox.Text;
-            titleEditBox.Text = "";
-            editTitleFlyout.Hide();
+            sender.TabItems.Remove(args.Tab);
         }
     }
 }
---
+++ DocumentTab.xaml.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Text;
+using System.Diagnostics;
+using Windows.Storage.Streams;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WritingAssistant
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DocumentTab : Page
+    {
+        public DocumentTab()
+        {
+            this.InitializeComponent();
+        }
+
+        
+
+        private void Editor_TextChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Editor_GotFocus(object sender, RoutedEventArgs e)
+        {
+            editor.Document.GetText(TextGetOptions.UseCrlf, out _);
+        }
+
+        private void TitleCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            titleEditBox.Text = "";
+            editTitleFlyout.Hide();
+        }
+
+        private void TitleConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            docTitle.Text = titleEditBox.Text;
+            titleEditBox.Text = "";
+            editTitleFlyout.Hide();
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Bold = FormatEffect.Toggle;
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            editor.Document.Selection.CharacterFormat.Italic = FormatEffect.Toggle;
+        }
+
+        private void ColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Extract the color of the button that was clicked.
+            Button clickedColor = (Button)sender;
+            var rectangle = (Microsoft.UI.Xaml.Shapes.Rectangle)clickedColor.Content;
+            var color = ((SolidColorBrush)rectangle.Fill).Color;
+
+            editor.Document.Selection.CharacterFormat.ForegroundColor = color;
+
+            fontColorButton.Flyout.Hide();
+            editor.Focus(FocusState.Keyboard);
+        }
+    }
+}
